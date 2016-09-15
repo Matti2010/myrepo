06dad1d8a303eb81cecd14a6d195cdd7310c4490
Matti2010 <mati.amsalem@gmail.com>
2016-09-15 12:10:17 +0300

    release 1

---
+++ openvpn_iptables.sh
@@ -0,0 +1,5 @@
+iptables -I FORWARD -i tun0 -o eth0 -s 10.8.0.0/24 -d 10.0.0.0/24 -m conntrack --ctstate NEW -j ACCEPT
+iptables -I FORWARD -i tun0 -o eth0 -s 10.8.0.0/24                        -m conntrack --ctstate NEW -j ACCEPT
+iptables -I FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -I POSTROUTING -o eth0 -s 10.8.0.0/24 -j MASQUERADE
+iptables -t nat -I POSTROUTING -o eth0 -s 10.0.0.0/24 -j MASQUERADE
